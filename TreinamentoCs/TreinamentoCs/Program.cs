a5652ef2abc22dad15f27a8f6622b917c9f89621
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-09-29 06:24:38 -0300

    FInalização do moudulo linguagem de programação C#

--- TreinamentoCs/TreinamentoCs/Program.cs
+++ TreinamentoCs/TreinamentoCs/Program.cs
@@ -6,13 +6,42 @@ namespace TreinamentoCs
     {
         static void Main(string[] args)
         {
+            var mouse = new Product(1, "Leite", 2.55, EProductType.Product);
 
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.EProductType);
+            Console.WriteLine((int)mouse.EProductType);
 
-            float salario = 3.400f;
-
-            var di = 3.400;
         }
 
+    }
+
+    struct Product{
+
+        public Product(int id, string name, double price, EProductType eProductType) {
+            Id = id;
+            Name = name;
+            Price = price;
+            EProductType = eProductType;
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+        public EProductType EProductType;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
     }
 }
 
