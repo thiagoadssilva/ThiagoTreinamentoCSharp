ec0e6c2ffcde52f938ff78131252cc4427a182e7
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-10-02 05:58:49 -0300

    Finalização do projeto cronometro

---
+++ CalculatorImc/Program.cs
@@ -0,0 +1,21 @@
+﻿double peso, altura;
+
+Console.Clear();
+Console.WriteLine("******************Sistema para Calcular seu IMC******************");
+
+Console.WriteLine("Informe seu Peso");
+peso = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Informe sua Altura");
+altura = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+CalcularImc(peso, altura);
+
+
+static void CalcularImc(double altura, double peso)
+{
+    double resultado = peso / Math.Pow(altura, 2);
+    Console.WriteLine($"Seu IMC é de: {resultado}");
+    Console.ReadKey();
+}
---
+++ StopWatch/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("*********************Seu Cronometro**************************");
+Menu();
+
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S = Segundos => 10s = 10 Segundos");
+    Console.WriteLine("M = Minutos => 20m = 20 Minutos");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length -1, 1));
+    int time = int.Parse(data.Substring(0, data.Length -1));
+    int multiplier = 1;
+
+    if (type == 'm')
+    {
+        multiplier = 60;
+    }
+
+    if (time == 0)
+    {
+        System.Environment.Exit(0);
+    }
+
+    PreStart(time * multiplier);
+}
+
+static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready..");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Goo...");
+    Thread.Sleep(2000);
+    Console.Clear();
+    Start(time);
+}
+
+static void Start(int time)
+{
+    Console.Clear();
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+        Console.Clear();
+        Console.WriteLine($"Seu cronometro vai até {time}");
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+    Console.Clear();
+    Console.WriteLine("StopWatch Finalizado...");
+    Thread.Sleep(2500);
+    Menu();
+}
+
