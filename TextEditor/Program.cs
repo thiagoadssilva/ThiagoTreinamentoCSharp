8170ac7cf26dee667fe51e2179e6bea7e49fc6b5
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-10-04 04:51:11 -0300

    Projeto editor de texto

---
+++ TextEditor/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar um novo arquivo");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("-----------------------------------------------------");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+    }
+    
+}
+
+static void Abrir()
+{
+
+}
+
+static void Editar()
+{
+    Console.Clear();
+    Console.WriteLine("Digite seu texto abaixo e para finalizar click em ESC");
+    Console.WriteLine("-----------------------------------------------------");
+
+    string texto = "";
+
+    do
+    {
+        texto += Console.ReadLine();
+        texto += Environment.NewLine;
+    } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Console.WriteLine("-----------------------------------------------------");
+    Console.Write(texto);
+
+}
