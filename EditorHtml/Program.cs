dee8cf5b2e16047947ce70a4f5fa5d694700afaf
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-10-06 05:36:51 -0300

    Criando editor html

---
+++ EditorHtml/Program.cs
@@ -0,0 +1,3 @@
+﻿using EditorHtml;
+
+Menu.Show();
---
+++ EditorHtml/Viewer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHtml
+{
+    public static class Viewer
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÂO");
+            Console.WriteLine("-----------");
+            Start();
+        }
+    }
+}
